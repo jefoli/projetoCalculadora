43e5fe57ab7b0164e92d7710f18ec863b7ab74f1
Jeferson Oliveira <jheferson.contato@hotmail.com>
2022-12-13 14:51:03 -0300

    correção de dados

--- teste.js
+++ teste.js
@@ -1,94 +1,94 @@
 function criaCalculadora(){
-    return {
-
+    return{
 
         display: document.querySelector('.display'),
        
         
-
-        
         inicia(){
             this.cliqueBotoes();
-            this.pressionaEnter(); 
             this.clearDisplay();
+            this.pressionaEnter();
         },
 
         pressionaBackSpace(){
             this.display.addEventListener('keydown', e => {
-                if (e.keyCode === 8){
+                if(e.keyCode === 8){
                     e.preventDefault();
-                    this.clearDisplay()
+                    this.clearDisplay();
                 }
             });
         },
 
-        pressionaEnter() {
+        pressionaEnter(){
             this.display.addEventListener('keyup', e => {
-                if (e.keyCode === 13) {
+                if(e.keyCode === 13){
                     this.realizaConta();
                 }
             });
         },
-
+       
         clearDisplay(){
             this.display.value = '';
         },
 
+
         apagaUm(){
-            this.display.value = this.display.value.slice(0,-1); 
+            this.display.value = this.display.value.slice(0,-1);
         },
+        
 
         realizaConta(){
-
-
             let conta = this.display.value;
-            
+
             try {
                 conta = eval(conta);
-                if(!conta){
-                    alert('Conta Inválida');
-                    return; 
+                if(!conta) {
+                    alert('Conta inválida');
+                    return;
                 }
-                
+            
                 this.display.value = String(conta);
-
+            
             } catch(e){
-                alert('Conta Inválida');
+                alert('Conta inválida');
                 return;
             }
         },
-
+    
+    
         cliqueBotoes(){
-        
-            document.addEventListener('click', e => { 
-                const el = e.target;       
-                
-                if(el.classList.contains('btn-num')){
-                    this.btnParaDisplay(el.innerText); 
+            document.addEventListener('click', e =>{
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
                 }
-            
+
                 if(el.classList.contains('btn-clear')){
                     this.clearDisplay();
                 }
-                
+
                 if(el.classList.contains('btn-del')){
                     this.apagaUm();
                 }
-           
+
                 if(el.classList.contains('btn-eq')){
                     this.realizaConta();
                 }
-                
+
+            
                 this.display.focus();
             });
+
         },
-        
+
         btnParaDisplay(valor){
-            this.display.value += valor; 
-        }
-    
+            this.display.value =+ valor;
+        },    
     };
+
 }
+
 
 const calculadora = criaCalculadora();
 calculadora.inicia();
